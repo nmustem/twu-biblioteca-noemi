3211d9ffad5cb84ee5734377e21f867789b4a28c
noemi <noemi.muste@thoughtworks.com>
2018-09-13 21:01:12 +0200

    US:Successful/Unsuccessful Checkout - LibraryManager class created, checkoutBook method test @Test

---
+++ src/com/twu/biblioteca/LibraryManager.java
@@ -0,0 +1,12 @@
+package com.twu.biblioteca;
+
+import com.twu.biblioteca.dto.Book;
+
+import java.util.List;
+
+public class LibraryManager {
+
+    public Boolean checkoutBook(int bookId, List<Book> booksList) {
+        return null;
+    }
+}
---
+++ test/com/twu/biblioteca/LibraryManagerTest.java
@@ -0,0 +1,38 @@
+package com.twu.biblioteca;
+
+import com.twu.biblioteca.dto.Book;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class LibraryManagerTest {
+
+    private LibraryManager libraryManager = new LibraryManager();
+    private  List<Book> booksList;
+
+    @Before
+    public void setUpStreams() {
+        this.booksList = new ArrayList<Book>();
+        Book book1 = new Book(1, "Book 1", "Autor1", 1990, false);
+        Book book2 = new Book(2, "Book 2", "Autor2", 1991, false);
+
+        booksList.add(book1);
+        booksList.add(book2);
+    }
+
+    @Test
+    public void successfulCheckoutBookTest() {
+        assertTrue(libraryManager.checkoutBook(1, booksList));
+    }
+
+    @Test
+    public void failedCheckoutBookTest() {
+        assertFalse(libraryManager.checkoutBook(13, booksList));
+    }
+}
