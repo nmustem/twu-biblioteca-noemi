696bc65572c755e9e69b5a6c74779b314fedebf7
noemi <noemi.muste@thoughtworks.com>
2018-09-13 15:06:02 +0200

    Book DTO created @Development

---
+++ src/com/twu/biblioteca/dto/Book.java
@@ -0,0 +1,70 @@
+package com.twu.biblioteca.dto;
+
+public class Book {
+    private Integer id;
+
+    private String title;
+
+    private String author;
+
+    private int year;
+
+    private boolean rented;
+
+    public Book() {
+    }
+
+    public Book(Integer id, String title, String author, int year, boolean rented) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.rented = rented;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public boolean isRented() {
+        return rented;
+    }
+
+    public void setRented(boolean rented) {
+        this.rented = rented;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("| %5s | %-30s | %-30s | %4s | %5s |",
+                id, title.length() > 30 ? title.substring(0, 27) + "..." : title, author.length() > 30 ? author.substring(0, 27) + "..." : title, year, rented);
+    }
+}
