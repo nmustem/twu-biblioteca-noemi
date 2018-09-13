fe8acc2b14ab4ba9709c9ad961abb782e6b6e28d
noemi <noemi.muste@thoughtworks.com>
2018-09-13 21:20:31 +0200

    US:Successful/Unsuccessful Checkout - checkoutBook testing the list size @Development

--- src/com/twu/biblioteca/LibraryManager.java
+++ src/com/twu/biblioteca/LibraryManager.java
@@ -9,6 +9,7 @@ public class LibraryManager {
     public Boolean checkoutBook(int bookId, List<Book> booksList) {
         for(Book book: booksList){
             if(bookId == book.getId()){
+                booksList.remove(book);
                 return true;
             }
         }
