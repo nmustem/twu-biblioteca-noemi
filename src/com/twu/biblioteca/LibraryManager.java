9c2e0d83c70581cba7bf1ef03dedb2e5e596ac3c
noemi <noemi.muste@thoughtworks.com>
2018-09-13 23:19:52 +0200

    US:Successful/Unsuccessful return book - returnBook method @Development

--- src/com/twu/biblioteca/LibraryManager.java
+++ src/com/twu/biblioteca/LibraryManager.java
@@ -5,6 +5,7 @@ import com.twu.biblioteca.dto.Book;
 import java.util.List;
 
 public class LibraryManager {
+
     /**
      * Checks if there is a book with id=bookId inside the given list, if so, the rented flag of this book is updated
      * @param bookId the book id to checkout
@@ -15,6 +16,15 @@ public class LibraryManager {
         for (Book book : booksList) {
             if (bookId == book.getId() && !book.isRented()) {
                 book.setRented(true);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Boolean returnBook(int bookId, List<Book> booksList) {
+        for (Book book : booksList) {
+            if (bookId == book.getId() && book.isRented()) {
                 return true;
             }
         }
