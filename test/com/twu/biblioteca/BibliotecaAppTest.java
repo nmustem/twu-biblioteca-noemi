5f39b00f771fd3c3832e565b340e5747f0c34a15
noemi <noemi.muste@thoughtworks.com>
2018-09-13 12:25:25 +0200

    US:Welcome Message - Welcome message @Test

---
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -0,0 +1,32 @@
+package com.twu.biblioteca;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.*;
+
+public class BibliotecaAppTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void test() {
+        BibliotecaApp.main(new String[0]);
+        assertEquals("Welcome to the Bangalore Public Library. The Biblioteca is available to use it!\n", outContent.toString());
+    }
+
+}
